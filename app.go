c941396d84fa2eb960ff2553088dacb0c1a40f24
Duke <duke@riseup.net>
2014-09-04 20:18:19 -0300

    updated redis lib, added logger

--- app.go
+++ app.go
@@ -1,28 +1,37 @@
 package main
 
 import (
-	"log"
 	"net/url"
 	"os"
-	"strconv"
 	"time"
 
 	"github.com/ChimeraCoder/anaconda"
-	"menteslibres.net/gosexy/redis"
+	"github.com/op/go-logging"
+	"gopkg.in/redis.v2"
 )
 
 var api *anaconda.TwitterApi
 var client *redis.Client
+var logg = logging.MustGetLogger("ken")
+
+var format = "%{color}%{time:15:04:05.000000} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}"
 
 func main() {
-	client = redis.New()
+	client = redis.NewTCPClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_HOST") + ":" + os.Getenv("REDIS_PORT"),
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
 
-	portInt, _ := strconv.Atoi(os.Getenv("REDIS_PORT"))
-
-	err := client.Connect(os.Getenv("REDIS_SERVER"), uint(portInt))
+	logBackend := logging.NewLogBackend(os.Stderr, "", 0)
+	syslogBackend, err := logging.NewSyslogBackend("")
 	if err != nil {
-		log.Fatal(err)
+		logg.Fatal(err)
 	}
+	logging.SetBackend(logBackend, syslogBackend)
+	logging.SetFormatter(logging.MustStringFormatter(format))
+	logging.SetLevel(logging.DEBUG, "ken")
+	logg.Info("Starting")
 
 	anaconda.SetConsumerKey(os.Getenv("CONSUMER_KEY"))
 	anaconda.SetConsumerSecret(os.Getenv("CONSUMER_SECRET"))
@@ -31,12 +40,15 @@ func main() {
 
 	tweetC := make(chan anaconda.Tweet)
 	tweetTickerC := time.NewTicker(2 * time.Hour).C
+	go request(tweetC)
 
 	for {
 		select {
 		case tweet := <-tweetC:
+			logg.Info("Retwetted #{tweet.IdS}")
 			api.Retweet(tweet.Id, false)
 		case <-tweetTickerC:
+			logg.Info("Ticker")
 			go request(tweetC)
 		}
 	}
@@ -47,22 +59,25 @@ func request(tweetC chan<- anaconda.Tweet) {
 	v.Set("count", "10")
 	v.Set("since_id", getSinceID())
 
+	logg.Info("Search with " + v.Encode())
 	searchResult, err := api.GetSearch("\"quem curte\" -RT ?", v)
 	if err != nil {
 		return
 	}
+
+	logg.Info("Found " + string(len(searchResult)) + " Tweets")
 	for _, tweet := range searchResult {
 		tweetC <- tweet
 	}
 	if len(searchResult) > 0 {
-		client.Set("last_tweet_id", searchResult[0].Id)
+		client.Set("last_tweet_id", searchResult[0].IdStr)
 	}
 }
 
 func getSinceID() string {
-	sinceID, err := client.Get("last_tweet_id")
-	if err != nil {
+	result := client.Get("last_tweet_id")
+	if result.Err() != nil {
 		return "0"
 	}
-	return sinceID
+	return result.String()
 }
